d910fbf552bc5f5a726200d3aed63e05e730c9b4
Kristian Ionescu <ionescu.kristian@gmail.com>
2025-12-25 17:49:42 +0100

    wip

--- src/components/PageDocument.tsx
+++ src/components/PageDocument.tsx
@@ -9,37 +9,78 @@ export default function DocumentReader({ data }: BookReaderProps) {
   return (
     <main style={{ maxWidth: 800, margin: "0 auto", padding: "2rem" }}>
       {book.chapters.map((chapter, chapterIndex) => (
-        <section key={chapterIndex} style={{ marginBottom: "4rem" }}>
+        <section key={chapterIndex} style={{ marginBottom: "6rem" }}>
+
+          {/* Chapter separator */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              margin: "6rem 0 3rem",
+            }}
+          >
+            <span style={{ color: "#aaa", whiteSpace: "nowrap" }}>
+              第{chapterIndex + 1}章
+            </span>
+
+            <hr
+              style={{
+                flex: 1,
+                border: "none",
+                borderTop: "1px solid #370171ff",
+              }}
+            />
+          </div>
+
           {chapter.title && (
             <h2
               style={{
-                fontStyle: "oblique",
                 fontSize: "2.4rem",
                 fontWeight: 600,
-                marginBottom: "2rem",
+                marginBottom: "3rem",
               }}
             >
               {chapter.title}
             </h2>
           )}
 
-            {chapter.pages.map((page, pageIndex) => (
+          {chapter.pages.map((page, pageIndex) => (
             <article key={pageIndex}>
-                <div
+              <div
                 dangerouslySetInnerHTML={{ __html: page.html }}
                 style={{
-                    fontSize: "2.0rem",
-                    lineHeight: 2,
-                    marginBottom: "3rem",
+                  fontSize: "2.0rem",
+                  lineHeight: 2,
+                  marginBottom: "3rem",
                 }}
-                />
-                <hr style={{ border: "none", borderTop: "1px solid #444", margin: "4rem 0" }} />
-            </article>
-            ))}
+              />
 
+              {/* Page separator */}
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "1rem",
+                  margin: "4rem 0",
+                }}
+              >
+                <span style={{ color: "#aaa", whiteSpace: "nowrap" }}>
+                  ページ {pageIndex + 1}
+                </span>
+
+                <hr
+                  style={{
+                    flex: 1,
+                    border: "none",
+                    borderTop: "1px solid #444",
+                  }}
+                />
+              </div>
+            </article>
+          ))}
         </section>
       ))}
     </main>
   );
 }
-
